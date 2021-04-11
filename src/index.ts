be79d55ac0325312adabbb949ab045eff6e33eef
大澤元気 <oosawagenki@oosawagenkinoiMac.local>
2021-04-12 00:52:45 +0900

    server追加

--- src/index.ts
+++ src/index.ts
@@ -42,3 +42,13 @@ const resolvers = {
         }
     }
 }
+
+const server = new ApolloServer({ typeDefs, resolvers })
+
+const app = express()
+
+server.applyMiddleware({ app })
+
+app.listen({ port: 4000 }, () => {
+    console.log(`🚀Server ready at http://localhost:4000${server.graphqlPath}`)
+})
